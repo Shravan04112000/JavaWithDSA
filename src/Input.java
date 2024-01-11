4f8e74dcbdb1bc69059dcfef5d02b9d4464054bc
Shravan <shravan.70950@gmail.com>
2024-01-11 11:32:49 +0530

    Input

--- src/Input.java
+++ src/Input.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Input {
+    public static void main(String[] args) {
+        System.out.println("Enter your rollno");
+        Scanner input = new Scanner(System.in);
+        int rollno = input.nextInt();
+        System.out.println("Your Roll No is:"+ rollno);
+    }
 }
---
+++ src/Primitives.java
@@ -0,0 +1,8 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 66;
+        char letter = 's';
+        float num = 89.99f;
+
+    }
+}
